1d4bc25f1f8132d302cef96afa1250d0b95636e4
Diego <dtarazona@uniguajira.edu.co>
2022-03-17 23:58:03 -0500

    agregado api

--- Tarea3/menu.py
+++ Tarea3/menu.py
@@ -1,44 +1,17 @@
 # Menu persona 
-
-import os 
-
-def Menu():
-
-    print("Menu Principal:  \n 1. Mostrar Persona \n 2. Insertar Persona \n 3. Modificar Perona \n 4. Eliminar Persona \n 5. Salir")
-    
-    menuP=0
-    while menuP != 5:
-
-        menuP=int(input("Digite opcion =>  "))
-        os.system('cls')
-
-        if menuP == 1:
-            print("------------ Mostrar Persona ---------- \n")
-
-            print("|  Nombre  |  Apellido  |   Direccion  |  Telefono  |   Sueldo   |  Transporte | Alimentacion  |  Pension  |  Salud  | ")
+from api.menuP import *
+ 
+def listadoPersonas():
+    class Persona():
+        def __init__(self): 
+            self.nombre=input("Ingrese el nombre:   ")
+            self.apellido=input("Ingrese el apellido:  ")
+            self.direccion=input("Ingrese la direccion: ")
+            self.telefono=input("Ingrese el telefono:  ")
+        def mostrarListadoPer(self):
+            print(f"Nombre: {self.nombre}    Apellido: {self.apellido}")
 
 
-        elif menuP == 2:
-            print("------------ Insertar Persona ---------- \n")
-
-            print("|  Nombre  |  Apellido  |   Direccion  |  Telefono  |   Sueldo   |  Transporte | Alimentacion  |  Pension  |  Salud  | ")
-
-        elif menuP == 3:
-            print("------------ Modificar Persona ---------- \n")
-
-            print("|  Nombre  |  Apellido  |   Direccion  |  Telefono  |   Sueldo   |  Transporte | Alimentacion  |  Pension  |  Salud  | ")
-    
-        elif menuP == 4:
-            print("------------ Eliminar Persona ---------- \n")
-
-            print("|  Nombre  |  Apellido  |   Direccion  |  Telefono  |   Sueldo   |  Transporte | Alimentacion  |  Pension  |  Salud  | ")
-
-        elif menuP == 5:
-            print("5. Salir \n")
-        else:
-         print("Digite una opcion valida")    
-
-        
 
 
 def main():
